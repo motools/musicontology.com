f38af8df2b393b86507df6b01dd2b737bfa93f9d
Yves Raimond <yves.raimond@bbc.co.uk>
2013-09-21 23:14:42 +0100

    Moving to newest bootstrap

--- docs/phpspecgen.php
+++ docs/phpspecgen.php
@@ -223,16 +223,17 @@
 </head>
 <body onload="prettyPrint()">
     <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
-          </a>
-          <a class="brand" href="../index.html">Music Ontology</a>
-          <div class="nav-collapse collapse">
-            <ul class="nav">
+          </button>
+          <a class="navbar-brand" href="index.html">Music Ontology</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
               <li><a href="../index.html">Home</a></li>
               <li><a href="getting-started.html">Getting started</a></li>
               <li class="active"><a href="specification.html">Specification</a></li>
@@ -245,8 +246,8 @@
         </div>
       </div>
     </div>
-    <div class="container">
 
+    <div class="container">
 <?php
 
     // Parse the document
@@ -266,6 +267,6 @@
         print $vocab->htmlTerms('Phpspecgen_Property', 'Properties');
     }
 ?>
-    </div>
+</div>
 </body>
 </html>
