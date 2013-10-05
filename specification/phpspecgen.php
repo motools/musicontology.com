6349322077c38857719ae1234d56792a532f5c42
Yves Raimond <yves.raimond@bbc.co.uk>
2013-10-05 13:01:09 +0100

    Adding analytics bug

--- specification/phpspecgen.php
+++ specification/phpspecgen.php
@@ -235,6 +235,15 @@
     <script src="../js/bootstrap.min.js"></script>
     <script src="../js/prettify.js"></script>
     <script src="../js/mo.js"></script>
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        
+  ga('create', 'UA-44632233-1', 'musicontology.com');
+  ga('send', 'pageview');
+    </script>
 </head>
 <body onload="prettyPrint()">
     <div class="navbar navbar-inverse navbar-fixed-top">
