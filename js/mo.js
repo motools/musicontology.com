6a1de9cd6eea9ddc4f6bbca80c82cf37c8ef11f9
Yves Raimond <yves.raimond@bbc.co.uk>
2014-06-27 15:02:37 +0100

    Making sure we update the location on click

--- js/mo.js
+++ js/mo.js
@@ -146,6 +146,7 @@ $(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      location.hash = this.hash;
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top
